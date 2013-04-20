1e4b9e4e7d5dfbdd14a4e78397ce9ff0e7a89377
Ralph Bean <rbean@redhat.com>
2013-04-20 01:26:54 -0400

    Index page.

--- oculum/__init__.py
+++ oculum/__init__.py
@@ -2,18 +2,51 @@ import flask
 import hashlib
 import charts
 
+import sqlalchemy
+import datanommer.models as m
+m.init('postgres://datanommer:bunbunbun@localhost/datanommer')
+
 app = flask.Flask(__name__)
+
+def make_gravatar(username):
+    email = username + "@fedoraproject.org"
+    digest = hashlib.md5(email).hexdigest()
+    gravatar = "http://www.gravatar.com/avatar/%s" % digest
+    gravatar += "?s=128"
+    return gravatar
+
+@app.route('/')
+def index():
+    rows = 5
+    columns = 6
+    n = rows * columns
+
+    # TODO -- throw this away and return the 'top' n users.
+
+    # TODO -- make this a classmethod in datanommer
+    users = m.session.query(m.User)\
+        .order_by(sqlalchemy.func.random())\
+        .limit(n)\
+        .all()
+
+    gravatars = [
+        (user.name, make_gravatar(user.name)) for user in users
+    ]
+
+    return flask.render_template(
+        "index.html",
+        gravatars=gravatars,
+        rows=rows,
+        columns=columns,
+    )
 
 
 @app.route('/<username>/')
 def profile(username):
-    email = username + "@fedoraproject.org"
-    digest = hashlib.md5(email).hexdigest()
-    gravatar = "http://www.gravatar.com/avatar/%s" % digest
     return flask.render_template(
         "profile.html",
         username=username,
-        gravatar=gravatar,
+        gravatar=make_gravatar(username),
     )
 
 
